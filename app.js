ed9ce2d526957e830a241f9f164df4aceab3fce1
connorbohannan <connor.bohannan@klaviyo.com>
2021-10-06 23:57:16 -0400

    Last

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const Name = document.querySelector('#Name')
 // grab release date 
 const Released = document.querySelector('#Released')
+//grab poster
+const Poster = document.querySelector('#Poster')
 // grab runtime 
 const Runtime = document.querySelector('#Runtime')
 // grab director
@@ -16,76 +18,41 @@ const moviePlot = document.querySelector('#moviePlot')
 // grab rotten tomatoes 
 const rottenTomatoes = document.querySelector('#rottenTomatoes')
 
-
-// submit movieSearch form
+// create movieSearch function
 const movieSearch = (event) => {
     // prevent default behavior
     // event.preventDefault()
-    // grab Title box value
+     // grab Year box value
     const Year = document.querySelector('#Year').value
+     // grab Title box value
     const Title = document.querySelector('#Title').value
-    // grab Year box value
     // grab Plot value
     const Plot = document.querySelector('#Plot').value
     //api URL 
-    const url = `http://www.omdbapi.com/?s=${Title}&apikey=9fdeb5da`
+    const url = `http://www.omdbapi.com/?t=${Title}&y=${Year}&plot=${Plot}&apikey=9fdeb5da`
 
     fetch(url)
         .then(res => res.json())
         .then(res => {
             console.log(res)
-            console.log(res)
-            for (let i = 0; i < res.Search.length; i++) {
-                console.log(res.Search[i])
-                        Name.innerHTML = res.Search[i].Title
-                        Released.innerText = res.Search[i].Year 
-                        // Image.innerText = res.Search[i].Poster
-                    }
-            // tentative adding inner text to H3 tags
-            // Name.innerText = res.search.title 
-            // Released.innerText = res.
-            // Runtime.innerText = res.
-            // Director.innerText = res.
-            // Writer.innerText = res.
-            // Actors.innerText = res.
-            // moviePlot.innerText = res.
-            // rottenTomatoes.innerText = res.
-
-        })
-    
+                Name.innerText = `Title: ${res.Title}`
+                Released.innerText = `Year: ${res.Year}`
+                Writer.innerText = `Writer(s): ${res.Writer}`
+                Poster.src = `${res.Poster}`
+                Director.innerText= `Director: ${res.Director}`
+                moviePlot.innerText = `Plot: ${res.Plot}`
+                Runtime.innerText = `Runtime: ${res.Runtime}`
+                Actors.innerText = `Actors: ${res.Actors}` 
+                rottenTomatoes.innerText= `Rotten Tomatoes Score: ${res.Ratings[1].Value}`
+        }) 
 }
 
-
+// grab submit button 
 const submitButton = document.querySelector('#submit')
 
+// listen to click of submit button and run movieSearch function
 submitButton.addEventListener('click', (event) => {
     event.preventDefault()
     movieSearch()
     
 })
-
-// grab form
-// const form = document.querySelector('#movieSearch')
-
-// listen to form submit
-// movieSearch.addEventListener("submit", movieSearch)
-
-
-
-
-
-
-
-// fetch takes an argument which is the URL that we want to GET data from
-// fetch (url)
-//parse data into JS
-    // .then((res) => res.json())
-    // we write logic inside of what we want to happen below
-//     .then((res) => {
-//         console.log(res)
-//     for (let i = 0; i < XXXXXX.length; i++) {
-//         const movie = document.createElement('h3')
-//         movie.innerText = res.XXXXXX[i].name
-//         output.appendChild(movie)
-//     }
-// })
